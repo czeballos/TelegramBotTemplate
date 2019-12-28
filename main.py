219ef7968cf4918326eb5c20d5801e322c1f01af
Carlos Zeballos <carloszeballos.developer@gmail.com>
2019-12-28 14:03:31 -0400

    Added help menu

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 import os
 
 import telebot
+
+from utils.commands import *
 
 token = os.environ['token']
 bot = telebot.TeleBot(token)
@@ -9,6 +14,11 @@ bot = telebot.TeleBot(token)
 @bot.message_handler(commands='start')
 def start(message):
     bot.reply_to(message, 'Welcome!')
+
+
+@bot.message_handler(commands='help')
+def help(message):
+    bot.reply_to(message, HELP_MESSAGE)
 
 
 if __name__ == "__main__":
---
+++ utils/commands.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+"""
+utils.commands
+==============
+
+This module contains useful functions and variables for the bot.
+"""
+
+HELP_MESSAGE = '''
+List of available message
+/cmd1 - do something
+/cmd2 - do something
+/cmd3 - do something
+/cmd4 - do something
+/cmd5 - do something
+/cmd6 - do something
+/cmd7 - do something
+/cmd8 - do something
+/cmd9 - do something
+'''
