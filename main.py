1a32bde233a7365ed2b0b57e2b091ccc8e9e2b60
Carlos Zeballos <carloszeballos.developer@gmail.com>
2019-12-29 15:53:08 -0400

    It change the way how message are send to avoid resend the original message

--- main.py
+++ main.py
@@ -11,14 +11,19 @@ token = os.environ['token']
 bot = telebot.TeleBot(token)
 
 
-@bot.message_handler(commands='start')
+@bot.message_handler(commands=['start'])
 def start(message):
-    bot.reply_to(message, 'Welcome!')
+    # ================================================================================
+    # It respond including a reply of the original message.
+    # bot.reply_to(message, 'Welcome!')
+    # ================================================================================
+
+    bot.send_message(message.chat.id, 'Welcome!')
 
 
-@bot.message_handler(commands='help')
+@bot.message_handler(commands=['help'])
 def help(message):
-    bot.reply_to(message, HELP_MESSAGE)
+    bot.send_message(message.chat.id, HELP_MESSAGE)
 
 
 if __name__ == "__main__":
